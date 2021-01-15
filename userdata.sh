a876d3868050736bc9b1c8c1f9058699764a9127
Uulzhan <35740041+Uulzhan@users.noreply.github.com>
2021-01-15 02:18:57 -0600

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,11 @@
-#! /bin/bash
-    sudo yum update
-    sudo yum install -y httpd
-    sudo systemctl start httpd
-    sudo systemctl enable httpd
-    echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+#!/bin/bash
+sudo yum update -y
+sudo yum install git -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum upgrade
+sudo yum install -y jenkins java-1.8.0-openjdk-devel
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
